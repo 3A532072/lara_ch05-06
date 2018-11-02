ed9e13e462d1404fe398965b53e819a27f82ed74
3A532072 <dinel1138@gmail.com>
2018-11-03 00:38:09 +0800

    練習九:create and use StudentController

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', 'HomeController@index');
 /*
 Route::pattern('student_no','s[0-9]{10}');
@@ -29,15 +18,13 @@ Route::get('student/{student_no}/score/{subject}', function ($student_no,$subjec
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });*/
-/*
+
 Route::group(['prefix'=>'student'],function() {
-   Route::get('{student_no}',['as'=>'student','uses'=>function($student_no){
-            return '學號:'.$student_no;
+   Route::get('{student_no}',['as'=>'student','uses'=>'StudentController@getStudentData'
         }
-    ]);*/
- /*
-    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=>function($student_no,$subject = null){
-            return '學號:'.$student_no.'  的'.((is_null($subject))?'所有科目':$subject).'成績';
+    ]);
+ 
+    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=>'StudentController@getStudentScore'
         }
     ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
-});*/
+});
