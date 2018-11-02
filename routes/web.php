e2e7c1a5140cb7328076ebd25b2fdba852333a78
3A532072 <dinel1138@gmail.com>
2018-11-03 00:02:04 +0800

    練習七:路由命名('as'=>'')

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,12 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
 Route::group(['prefix'=>'student'],function() {
-    Route::get('{student_no}', function ($student_no) {
-        return "學號:" . $student_no;
-    });
-    Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
-        return "學號:" . $student_no . "的" . ((is_null($subject)) ? "所有科目" : $subject) . "成績";
-    })->where(['(chinese|english|math)']);
+   Route::get('{student_no}',['as'=>'student','uses'=>function($student_no){
+            return '學號:'.$student_no;
+        }
+    ]);
+    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=>function($student_no,$subject = null){
+            return '學號:'.$student_no.'  的'.((is_null($subject))?'所有科目':$subject).'成績';
+        }
+    ])->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math)']);
 });
